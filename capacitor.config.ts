b747f707ecb030a9572f97c9f68ba01123a194e8
bneuhausz <bneuhausz@gmail.com>
2025-07-21 12:53:37 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'training-log-poc',
+  webDir: 'www'
+};
+
+export default config;
